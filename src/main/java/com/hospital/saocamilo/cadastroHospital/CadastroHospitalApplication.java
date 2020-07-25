9b32dd3e3ef5d50f253a1f042c7e2b2fc1f1dfe9
Gilmar Cruz Silva <supervingue67@gmail.com>
2020-07-25 15:40:57 -0300

    Primeiro commit cadastro para hospital

---
+++ src/main/java/com/hospital/saocamilo/cadastroHospital/CadastroHospitalApplication.java
@@ -0,0 +1,13 @@
+package com.hospital.saocamilo.cadastroHospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroHospitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroHospitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hospital/saocamilo/cadastroHospital/CadastroHospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.saocamilo.cadastroHospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroHospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
