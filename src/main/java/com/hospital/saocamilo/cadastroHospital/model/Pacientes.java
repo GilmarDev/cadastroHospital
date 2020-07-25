dfea8d0e3102091e74d20f9c4265ba40e3949bed
Gilmar Cruz Silva <supervingue67@gmail.com>
2020-07-25 17:15:12 -0300

    Criado a classe o model para pacientes

--- src/main/java/com/hospital/saocamilo/cadastroHospital/model/Pacientes.java
+++ src/main/java/com/hospital/saocamilo/cadastroHospital/model/Pacientes.java
@@ -1,5 +1,167 @@
 package com.hospital.saocamilo.cadastroHospital.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pacientes")
 public class Pacientes {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	
+	@Column(name = "nome")
+	private String nome;
+	
+	@Column(name = "idade")
+	private int idade;
+	
+	@Column(name = "nome_responsavel")
+	private String nomeResponsavel;
+	
+	@Column(name = "telefone")
+	private String telefone;
+	
+	
+	@Column(name = "email")
+	private String email;
+	
+	
+	@Column(name = "endereco")
+	private String endereco;
+
+
+	public long getId() {
+		return id;
+	}
+
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public int getIdade() {
+		return idade;
+	}
+
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+
+	public String getNomeResponsavel() {
+		return nomeResponsavel;
+	}
+
+
+	public void setNomeResponsavel(String nomeResponsavel) {
+		this.nomeResponsavel = nomeResponsavel;
+	}
+
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((endereco == null) ? 0 : endereco.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + idade;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((nomeResponsavel == null) ? 0 : nomeResponsavel.hashCode());
+		result = prime * result + ((telefone == null) ? 0 : telefone.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pacientes other = (Pacientes) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (endereco == null) {
+			if (other.endereco != null)
+				return false;
+		} else if (!endereco.equals(other.endereco))
+			return false;
+		if (id != other.id)
+			return false;
+		if (idade != other.idade)
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (nomeResponsavel == null) {
+			if (other.nomeResponsavel != null)
+				return false;
+		} else if (!nomeResponsavel.equals(other.nomeResponsavel))
+			return false;
+		if (telefone == null) {
+			if (other.telefone != null)
+				return false;
+		} else if (!telefone.equals(other.telefone))
+			return false;
+		return true;
+	}
+	
+	
 }
